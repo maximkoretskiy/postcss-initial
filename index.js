30da9b5130f89f0e45cfb89e6c142e7e7c51a643
Maksim Koretskiy <maksim.koretskiy@saltpepper.ru>
2015-08-24 22:51:50 +0300

    Little refactor

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
 var postcss = require('postcss');
-var rulesCollection = require('./lib/resetRules');
-
-function intersection(array1, array2) {
-  return array1.filter( function (n) {
-    return array2.indexOf(n) !== -1;
-  });
-}
-
-function getRules(types, rules) {
-  types = types || ['all'];
-  if( types.indexOf('all') !== -1 ) {
-    return rules;
-  }
-  return rules.filter( function (rule) {
-    return rule.type !== undefined && intersection(rule.type, types).lenght;
-  });
-}
-
-function clearRules(rules) {
-  return rules.map(function (rule) {
-    return {
-      prop:  rule.prop,
-      value: rule.value
-    };
-  });
-}
+var rulesFabric = require('./lib/rules-fabric');
 
 module.exports = postcss.plugin('postcss-all-unset', function (opts) {
   opts = opts || {};
-  var resetRules = clearRules(getRules(opts.reset, rulesCollection));
+  var resetRules = rulesFabric(opts.reset);
   return function (css) {
     css.walkDecls('all', function (decl) {
       if(decl.value !== 'unset') return;
